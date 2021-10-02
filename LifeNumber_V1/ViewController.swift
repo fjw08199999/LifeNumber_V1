80331fe59b3fffac4142dd94fd6500aa6cd63ead
fred.fu <fjw08199999@gmail.com>
2021-10-02 16:10:31 +0800

    Initial Commit

---
+++ LifeNumber_V1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeNumber_V1
+//
+//  Created by fred fu on 2021/10/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
